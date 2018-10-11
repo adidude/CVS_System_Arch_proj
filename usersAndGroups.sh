947fa9e94845b99326dc48a342db01c427adc787
Mandar Tamhane <m.tamhane@dundee.ac.uk>
2018-10-11 13:06:59 +0100

    Syntax Fixes

--- usersAndGroups.sh
+++ usersAndGroups.sh
@@ -3,38 +3,44 @@ createGroup()
 {
     echo -n "Enter name of the new group you would like: "
     read groupName
-    sudo groupadd $groupName
+    if [ "$(getent group $groupName)" ]; then
+        echo "Group $groupName already exists"
+    else
+        sudo groupadd $groupName
+    fi
 }
 
 #Add Users to Group
 addToGroup()
 {
     echo -n "Which group would you like to add users to?"
-    read groupName;
-    if [ $(getent group $groupName) ]; then
-        addUsers($groupName)
-    else 
+    read groupName
+    if [ "$(getent group $groupName)" ]; then
+        addUsers $groupName
+    else
+    {
         echo "Group doesn't exist. Create the group first."
+    }
     fi
 }
 
 #Add users
-addUsers($groupName)
+addUsers "$groupName"
 {
     echo "How many users do you want to add to this group: "
     read noOfUsers
 
-    while [$noOfUsers!=0]
+    while [ "$noOfUsers" != 0 ]
     do
         echo "Enter name of user"
         read newUser
-        if [ grep -c $newUser /etc/passwd ]; then
+        if [ -z "$(getent passwd $newUser)" ]; then
             sudo usermod -a -G $groupName $newUser
             echo "User $newUser was added to group $groupName"
         else
             echo "User $newUser couldn't be added to the group because the user doesn't exist. Create user first? Y/N: "
             read decision
-            if [ confirm($decision) ]; then
+            if confirm $decision ; then
                 echo "Enter the name for the user"
                     read userName
                     sudo useradd -m $userName
@@ -45,9 +51,9 @@ addUsers($groupName)
 }
 
 #View Members in Group
-viewMembers($groupName)
+viewMembers $groupName
 {
-    if [ $(getent group $groupName) ]; then
+    if [ "$(getent group $groupName)" ]; then
         grep $groupName /etc/group
     else 
         echo "Group doesn't exist. Create the group first."
@@ -55,18 +61,18 @@ viewMembers($groupName)
 }
 
 #View Owner of Directory
-viewOwner($path)
+viewOwner "$path"
 {
-    ls -l $path
+    ls -l "$path"
 }
 
 #Share file with group
-shareFile($path)
+shareFile "$path"
 {
     echo "Which group would you like to share this file with?"
     read groupName;
     
-    if [ $(getent group $groupName) ]; then
+    if [ "$(getent group $groupName)" ]; then
         echo "Which type of Permissions would you like to give to the file or folder?"
         echo "1. Read-Only"
         echo "2. Read & Write"
@@ -75,34 +81,49 @@ shareFile($path)
         echo "Enter your choice: "
         read choice
         case $choice in
-            1) chmod 5740 $groupName $path
+            1) chmod 7740 "$groupName" "$path"
                 ;;
-            2) chmod 5720 $groupName $path
+            2) chmod 7720 "$groupName" "$path"
                 ;;
-            3) chmod 5750 $groupName $path
+            3) chmod 7750 "$groupName" "$path"
                 ;;
-            4) chmod 5770 $groupName $path
+            4) chmod 7770 "$groupName" "$path"
                 ;;
         esac
-    else echo "Group doesn't exist"
+    else
+        echo "Group doesn't exist. Create the group first? Y/N?"
+        read decision
+            if confirm $decision ; then
+                createGroup
+            else return
+            fi
+        
     fi     
 }
 
 #View files shared with group
-viewFiles($groupName, $directoryName)
+viewFiles "$groupName" "$directoryName"
 {
-    find $directoryLocation -group {$groupName}
+    find "$directoryName" -group "$groupName"
 }
 
 #Check if Y or N
-confirm ($decision)
+confirm $decision
 {
     case $decision in
     [yY][eE][sS]|[yY])
-        true
+        return 1
         ;;
     *)
-        false
+        return 0
         ;;
     esac
+}
+
+#Check If User Exists
+#citation: https://ubuntuforums.org/showthread.php?t=1792819
+UserExist()
+{
+   awk -F":" '{ print $1 }' /etc/passwd | grep -x $1 > /dev/null
+   return $?
 }
